8fb329000552efedd9e986e8730476de43e2d580
gabrielmarianopendas <gabriel.pendas@gmail.com>
2022-01-17 10:13:24 -0700

    MasterMind

--- ruby_projects/computer_code_break.rb
+++ ruby_projects/computer_code_break.rb
@@ -3,31 +3,55 @@ module CompCodebreak
   def initialize
     @total_codes = []
     (1111..6666).each {|num| @total_codes << num}
-    @comp_set = @total_codes.dup
+    @comp_set = @total_codes.dup.uniq
   end
 
   def first_move
     @comp_move = 1122
   end
-
-  def player_code
-    puts "Please enter your secret code in a 1234 format."
-    @play_code = gets.chomp.to_i
+    
+  def player_code_set
+    @player_code = []
+    puts "Please enter your super secret code in a 1234 format: "
+    @player_code = gets.chomp.to_i
+    @player_code@player_code.digits
+    if @player_guess.to_s.size == 4 && @player_guess.to_i == @player_guess
+      true
+    else
+      puts "Please follow directions. Enter your guess in a 1234 format."
+      false
+    end
   end
     
+  def comp_hint
+    @num_hint = []
+    (0..3).each do |i|
+      if  @comp_move[i] == @player_move[i]
+        @num_hint << 'Y'
+      elsif @player_move.include?(@comp_move[i])
+        @num_hint << 'A'
+      end
+    end
+    @num_hint = @num_hint.sort.reverse
+  end
 
-  def comp_code_cull #need to change this so hint array is generated for each number
+  def comp_code_cull #need to change this so hint array is generated for each number    
     @comp_set.each do |num|
-      if num.hint == @hint_array
+      if @num_hint == @hint_array
         next
       else
         @comp_set.delete(num)
       end
     end
-    p comp_set
+    p @comp_set
   end
 
+  def which_side?
+    puts "Would you like to be the codemaker or codebreaker? Please enter maker or breaker."
+    @choice = gets.chomp
+  end
 
+  
   
 end
 
--- ruby_projects/mastermind.rb
+++ ruby_projects/mastermind.rb
@@ -1,67 +1,85 @@
-require './computer_code_break.rb'
+#require './computer_code_break.rb'
 
 class MasterMind
- include CompCodebreak
+  #include CompCodebreak
 
   def secret_code 
-    @comp_move = []
-    4.times do
-      @comp_move << rand(6)
+     @comp_move = []
+     4.times do
+       @comp_move << rand(1..6)
     end
     @comp_move
   end
 
-  def which_side?
-    puts "Would you like to be the codemaker or codebreaker? Please enter maker or breaker."
-    @choice = gets.chomp
+  def player_choice
+    puts "Would you like to be the codemaker or codebreaker?\nType in 'maker' or 'breaker'"
+    @play_choice = gets.chomp
+    @play_choice.downcase == 'breaker' ? true : false
   end
 
+  def number_of_rounds
+    loop do 
+      puts "How many rounds would you like to play? Choose and even number between 8-12"
+      @rounds_number = gets.chomp.to_i
+      round_arr = [8,10,12]
+      break if round_arr.include?(@rounds_number)
+    end
+    @rounds_number
+  end
 
+  def round_int
+    @round_number = @rounds_number
+  end
 
   def player_move
-    @player_move = []
+    @player_mov = []
     puts "Please enter your guess in a 1234 format: "
-    @player_guess = gets.chomp.to_i
-    @player_guess.digits.each {|digit| @player_move << digit.to_i}
-    p @player_guess
-    if @player_guess.to_s.size == 4 && @player_guess.to_i == @player_guess
-      true
-    else
-      puts "Please follow directions. Enter your guess in a 1234 format."
-      false
-    end
+    @player_guess = gets.chomp
+    @player_mov = @player_guess.chars.map(&:to_i)
   end
 
   def winner?
-    if @player_move == @comp_move 
-      puts "YOU DID IT! YOU BROKE THE CODE!!!"
-        true
-    else
-      false
+    if @comp_move.join.to_i == @player_mov.join.to_i
+      puts "Winner, winner, chicken dinner."
+      true
     end
   end
 
   def hint
-    @hint_array = []
-    (0..3).each do |i|
-      if @player_move[i] == @comp_move[i]
-        @hint_array << 'Y'
-      elsif @comp_move.include?(@player_move[i])
-        @hint_array << 'A'
+    matches = []
+    partial = []
+    p @comp_move
+    p @player_mov
+    pl_move = @player_mov.dup
+    pl_move.each_with_index do |num, i|
+      if num == @comp_move[i]
+        matches << num 
+      elsif @comp_move.include?(num)
+        partial << num unless matches.include?(num)
       end
     end
-    @hint_array.sort.reverse
+    puts "Matches: #{matches.size}"
+    puts "Partials: #{partial.size}"
   end
 
 
 end
 
+
 game = MasterMind.new
-p game.secret_code
-while game.player_move == false
-  game.player_move
+
+if game.player_choice
+  game.secret_code
+  game.number_of_rounds
+  counter = game.round_int
+  game.round_int.times do
+    game.player_move
+    game.hint unless game.winner?
+    counter -= 1
+    puts "You have #{counter} guesses remaining"
+  end
 end
-game.winner?
-p game.hint
-game.comp_code_cull
+  
+
+
 
